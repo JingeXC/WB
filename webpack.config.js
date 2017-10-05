6620521e65034c642336b6d9b6fd00ed9d39d64c
陈金歌 <cbb467cp01@163.com>
2017-10-05 21:55:26 +0800

    第一章

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+	devtool: 'inline-source-map',
+	devServer:{
+		contentBase: './dist'
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			title: 'OutPut Management'
+		}),
+		new CleanWebpackPlugin(['dist'])
+	],
+	output:{
+		filename: '[name]bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/'
+	}
+};
