c9ef3168fac184c2bf7fda43553b44b12b18da67
陈金歌 <cbb467cp01@163.com>
2017-10-06 19:33:54 +0800

    part2

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
 	entry: {
-		app: './src/index.js',
-		print: './src/print.js'
+		app: './src/index.js'
 	},
 	devtool: 'inline-source-map',
 	devServer:{
-		contentBase: './dist'
+		contentBase: './dist',
+		hot :true
+	},
+	module: {
+		rules:[
+			{
+				test: /\.css$/,
+				use: ['style-loader','css-loader']
+			}
+		]
 	},
 	plugins:[
 		new HtmlWebpackPlugin({
 			title: 'OutPut Management'
 		}),
-		new CleanWebpackPlugin(['dist'])
+		new CleanWebpackPlugin(['dist']),
+		new webpack.HotModuleReplacementPlugin()
 	],
 	output:{
 		filename: '[name]bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/'
+		path: path.resolve(__dirname, 'dist')
 	}
 };
